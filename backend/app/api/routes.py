ead8ff1d062f71f996968b65942179705bbe0a25
Xzhyan <malcondev.lds@hotmail.com>
2026-03-08 15:36:54 -0300

    new

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+# Rota de teste
 @router.get("/home")
 def home():
     return {'message': "Olá mundo!"}
 
 @router.post("/user/register")
 def user_register():
-    return
+    return 
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,6 @@ from backend.app.core.middleware import register_middleware
 from backend.app.api.routes import router
 from backend.app.database.session import engine
 from backend.app.database.base import Base
-import backend.app.models
 
 def create_app() -> FastAPI:
     app = FastAPI()
